57967473c2ab2772b23dfdc0e805fefe9a928df1
Magnus Fjell <magnus.fjell@bekk.no>
2014-05-12 15:02:30 +0200

    Spike for gallery widget for /Api/Content

---
+++ viewmodels/galleri.js
@@ -0,0 +1,20 @@
+﻿define(function(require){
+    var http = require('plugins/http')
+    var ko = require('knockout');
+    var _ = require('underscore');
+
+
+    var urls = ko.observableArray([]);
+
+    return {
+    	urls: urls,
+
+    	activate: function(id) {
+    		var that = this;
+            return http.get("/Api/Content/"+ id).then(function (response) {
+            	var files = _.flatten(_.pluck(response.Content, "Files"), true);
+            	that.urls(_.pluck(files, "Url"));
+            });
+    	}
+    }
+});
