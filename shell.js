1d102a25e1832e7dbf3506905be24a8900492897
Magnus Fjell <magnus.fjell@bekk.no>
2014-05-14 16:00:08 +0200

    Add search widget/app for api

--- shell.js
+++ shell.js
@@ -14,7 +14,8 @@
                 { route: 'Ansvarsarter', title: 'Ansvarsarter', moduleId: 'viewmodels/ansvarsarter', nav: true },
                 { route: 'ArtsnavnImport', title: 'ArtsnavnImport', moduleId: 'viewmodels/artsnavnImport', nav: true },
                 { route: 'Flervalg', title: 'Flervalg', moduleId: 'viewmodels/flervalg', nav: true },
-                { route: 'Galleri/:id', title: 'Galleri', moduleId: 'viewmodels/galleri', nav: true}
+                { route: 'Galleri/:id', title: 'Galleri', moduleId: 'viewmodels/galleri', nav: true},
+                { route: 'Søk', title: 'Søk', moduleId: 'viewmodels/search', nav: true}
             ]).buildNavigationModel();
 
             return router.activate();
---
+++ viewmodels/search.js
@@ -0,0 +1,73 @@
+﻿define(function(require){
+    var ko = require("knockout");
+    var _ = require("underscore");
+    var http = require('plugins/http');
+
+    var search = ko.observable("");
+    var items = ko.observableArray([]);
+
+    var gridViewModelSettings = {
+        data: items,
+        columns: [
+            {
+                headerText: "Vitenskapelignavn",
+                isSortable: true,
+                rowText: function(item) {
+                    return _.map(item.scientificNames,
+                            function(name){
+                                return name.scientificName
+                            }).join("\n") },
+                classList: "break-line"
+            },
+            {
+                headerText: "Author",
+                isSortable: true,
+                rowText: function(item) {
+                    return _.map(item.scientificNames,
+                            function(name){
+                                return name.scientificNameAuthorship
+                            }).join("\n") },
+                classList: "break-line"
+            },
+            {
+                headerText: "Rank",
+                isSortable: true,
+                rowText: function(item) {
+                    return _.map(item.scientificNames,
+                            function(name){
+                                return name.taxonRank
+                            }).join("\n") },
+                classList: "break-line"
+            },
+        ],
+        pageSize: 25,
+        currentPageIndex: ko.observable(0),
+        sortBy: function (column) {
+            if (!column.sortAsc) {
+                column.sortAsc = true;
+                items(_.sortBy(items(), column.rowText));
+            } else {
+                column.sortAsc = false;
+                items(_(items()).sortBy(column.rowText).reverse().value());
+            }
+        }
+    };
+
+    ko.computed(function(){
+        if(search().length < 3){ return; }
+
+        var hit = http.get("/Api/Taxon/", {scientificName: search()}).then(function (response) {
+            console.log(response);
+            items(response);
+        });
+    });
+
+    return {
+        search: search,
+        gridViewModelSettings: gridViewModelSettings,
+
+        activate : function() {
+
+        }
+    }
+})
