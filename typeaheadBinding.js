7e940edf5745bf892e46f7e03de10aa12795d6e3
Magnus Fjell <magnus.fjell@bekk.no>
2014-06-10 13:41:12 +0200

    Add ko binding for typeahead

---
+++ typeaheadBinding.js
@@ -0,0 +1,26 @@
+﻿define(function(require) {
+    var ko = require('knockout');
+    var $ = require('jquery');
+    ko.bindingHandlers.typeahead = {
+        init: function (element, valueAccessor, allBindingsAccessor) {
+            var $element = $(element);
+            var allBindings = allBindingsAccessor();
+            var typeaheadOpts = {};
+            var source = { source: ko.utils.unwrapObservable(valueAccessor()) };
+
+            triggerChange = function() {
+                $element.change();
+            }
+
+            if (allBindings.typeaheadOptions) {
+                $.each(allBindings.typeaheadOptions, function (optionName, optionValue) {
+                    typeaheadOpts[optionName] = ko.utils.unwrapObservable(optionValue);
+                });
+            }
+
+            $element.attr("autocomplete", "off").typeahead(typeaheadOpts, source )
+                .on("typeahead:selected", triggerChange)
+                .on("typeahead:autocompleted", triggerChange);
+        }
+    };
+});
