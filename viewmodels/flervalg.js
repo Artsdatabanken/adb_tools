f7832e860c946579e3a78323b6a10eaf6402b076
Torstein Altin <torstein.altin@artsdatabanken.no>
2016-02-08 09:56:09 +0100

    Remove bugs from flervalg

--- viewmodels/flervalg.js
+++ viewmodels/flervalg.js
@@ -1,7 +1,9 @@
-define(function(require){
+define(function (require) {
     var http = require('plugins/http')
     var ko = require('knockout');
     var _ = require('underscore');
+
+    var widgetPath = 'http://data.artsdatabanken.no/';
 
     var characters = ko.observableArray();
     var parents = ko.observableArray();
@@ -12,16 +14,18 @@ define(function(require){
     var dependencies = ko.observableArray();
     var selectedCharacters = {};
 
-    var filterParents = function(list) {
-        return _.filter(list, function(item){ return !item.parent; });
+    var filterParents = function (list) {
+        return _.filter(list, function (item) {
+            return !item.parent;
+        });
     };
 
-    var filterConditionsCharacter = function(characters) {
-        return _.filter(characters, function(character){
-            if(!_.isEmpty(character.conditions)){
+    var filterConditionsCharacter = function (characters) {
+        return _.filter(characters, function (character) {
+            if (!_.isEmpty(character.conditions)) {
                 var states = selectedStates().concat(implicitStates());
                 var intersection = _.intersection(states, character.conditions);
-                if(_.isEmpty(intersection)){//Do not show if it has conditions, but none of them are chosen
+                if (_.isEmpty(intersection)) {//Do not show if it has conditions, but none of them are chosen
                     return false;
                 }
                 return true;
@@ -30,56 +34,59 @@ define(function(require){
         });
     };
 
-    var findStatesCurrentItems = function() {
+    var findStatesCurrentItems = function () {
         //Find all stateIds for currently shown items, and for children if applicable.
         //Parents do not have their own stateIds, only their children have
-        return  _.map(currentItems(), function(item){
+        return _.map(currentItems(), function (item) {
+            var states = [], children;
 
-            //Will work without children as well, just a noop then
-            var children = item.children;
-            children = _.filter(items(), function(item){
-                return _.contains(children, item.id);
-            });
-            var states = _.flatten(children, "stateIds");
+            if (Array.isArray(item.children) && item.children.length) {
+                states = _.map(_.filter(items(), function (item) {
+                    return _.includes(children, item.id);
+                }), "stateIds");
+            } else {
+                states = item.stateIds;
+            }
 
-            //No children, use own states instead
-            if(states.length === 0) { states = item.stateIds }
             return states;
         });
     };
 
-    var charactersToShow = ko.computed(function() {
+    var charactersToShow = ko.computed(function () {
         var statesCurrentIds = findStatesCurrentItems();
-
         //statesCurrentIds is a list of lists, that contain all the states for each item
-        var filteredCharacters = _.filter(characters(), function(character){
+        var filteredCharacters = _.filter(characters(), function (character) {
             var stateIds = _.map(character.states, "id");
 
             //For a character to be shown, it has to have one of its states in all of the states for the current items
-            return _.all(statesCurrentIds, function(state) {
-                return ! _.isEmpty(_.intersection(state, stateIds));
+            return _.every(statesCurrentIds, function (state) {
+                return !_.isEmpty(_.intersection(state, stateIds));
             });
         });
 
         return filterConditionsCharacter(filteredCharacters);
     });
 
-    var filteredItems = ko.computed(function() {
-        if(selectedStates().length === 0){
+    var filteredItems = ko.computed(function () {
+        if (selectedStates().length === 0) {
             currentItems(parents());
             return;
         }
 
         //Find items where all the states that are selected, are in the statelist for the item
-        var filtered = _.filter(items(), function(item) {
+        var filtered = _.filter(items(), function (item) {
             var states;
             //If only 1 taxon remains, all states are implicitly selected, as they all 'belong' to the taxon.
             //Therefore we can't use the implicitly selected states
-            if(currentItems().length === 1) { states = selectedStates(); }
-            else { states = selectedStates().concat(implicitStates()); }
-            return _(states).all(function(state){
-                    return _.contains(item.stateIds, state)
-                });
+            if (currentItems().length === 1) {
+                states = selectedStates();
+            }
+            else {
+                states = selectedStates().concat(implicitStates());
+            }
+            return _(states).every(function (state) {
+                return _.includes(item.stateIds, state)
+            });
         });
 
         var mergedChildren = combineSiblingsIntoParent(filtered);
@@ -87,15 +94,15 @@ define(function(require){
         currentItems(mergedChildren);
     });
 
-    ko.computed(function() {
+    ko.computed(function () {
         var statesCurrentIds = findStatesCurrentItems();
         var groups = _.groupBy(_.flatten(statesCurrentIds));
         var keys = _.keys(groups); //All unique states for current items
 
-        var implicitlySelectedStates = _.filter(groups, function(list) {
+        var implicitlySelectedStates = _.filter(groups, function (list) {
             var currentState = list[0]; //Always at least one item, all items are the same
             var currentChar = currentState.split("_")[0];
-            var hasSiblingState = _.any(keys, function(state) {
+            var hasSiblingState = _.some(keys, function (state) {
                 var stateChar = state.split("_")[0];
                 return stateChar === currentChar && state !== currentState
             });
@@ -103,57 +110,67 @@ define(function(require){
             return list.length === currentItems().length && !hasSiblingState;
         });
 
-        implicitlySelectedStates = _.map(implicitlySelectedStates, function(list){ return list[0]; });
-        implicitlySelectedStates = _.filter(implicitlySelectedStates, function(state){ return ! _.contains(selectedStates(), state); });
+        implicitlySelectedStates = _.map(implicitlySelectedStates, function (list) {
+            return list[0];
+        });
+        implicitlySelectedStates = _.filter(implicitlySelectedStates, function (state) {
+            return !_.includes(selectedStates(), state);
+        });
         implicitStates(implicitlySelectedStates);
     });
 
-    var combineSiblingsIntoParent = function(listOfItems) {
+    var combineSiblingsIntoParent = function (listOfItems) {
         var itemIds = _.map(listOfItems, "id");
         var parentsWithAllChildrenInList = [];
 
-        _.forEach(parents(), function(parent) {
-            if(parent.children.length === 0 ) { return; }
+        _.forEach(parents(), function (parent) {
+            if (parent.children.length === 0) {
+                return;
+            }
 
             //Sjekk om alle ungene til parenten skal vises
-            var allChildrenInList = _.all(parent.children, function(childId) { return _.contains(itemIds, childId)});
+            var allChildrenInList = _.every(parent.children, function (childId) {
+                return _.includes(itemIds, childId)
+            });
 
-            if(allChildrenInList){
+            if (allChildrenInList) {
                 parentsWithAllChildrenInList.push(parent);
             }
         });
 
         var childrenToRemove = _.flatten(parentsWithAllChildrenInList, "children");
 
-        listOfItems = _.filter(listOfItems, function(item){ return ! _.contains(childrenToRemove, item.id)});
+        listOfItems = _.filter(listOfItems, function (item) {
+            return !_.includes(childrenToRemove, item.id)
+        });
         listOfItems = _.unique(listOfItems.concat(parentsWithAllChildrenInList));
 
         return listOfItems;
     };
 
-    var valgteOrdener = ko.computed(function(){
-        if(currentItems().length === 0) {
+    var valgteOrdener = ko.computed(function () {
+        if (currentItems().length === 0) {
             return parents();
         }
 
-        var selectedParents = _.map(currentItems(), function(item){
-            if(!item.parent){ //Hvis den er valgt, men ikke har noe parent, er den sin egen 'orden'
+        var selectedParents = _.map(currentItems(), function (item) {
+            if (!item.parent) { //Hvis den er valgt, men ikke har noe parent, er den sin egen 'orden'
                 return item.id;
             }
             return item.parent;
         });
-        return _.filter(parents(), function(parent){
-            return _.contains(selectedParents, parent.id);
+        return _.filter(parents(), function (parent) {
+            return _.includes(selectedParents, parent.id);
         });
     });
 
-    var stateSelected = function(data, event) {
+    var stateSelected = function (data, event) {
         settInnStateIListeOgOppdaterSelectedStates(data.id);
     };
 
-    var settInnStateIListeOgOppdaterSelectedStates = function(state) {
+    var settInnStateIListeOgOppdaterSelectedStates = function (state) {
         var character = state.split("_")[0];
-        if(selectedCharacters[character] === state){//State er allerede valgt, klikk på den skal fjerne valgt.
+        if (selectedCharacters[character] === state) {//State er allerede valgt, klikk på den skal fjerne valgt.
             delete selectedCharacters[character];
         } else {
             selectedCharacters[character] = state; //Sett inn state på character, og overskriv hvis søskenstate er valgt.
@@ -180,38 +197,43 @@ define(function(require){
                 var taxa = response.taxa.taxon;
                 var dependencies = response.dependencies.dependency;
                 var states = response.states.state;
-                var characters =  response.characters.character;
+                var characters = response.characters.character;
 
 
-                _.forEach(taxa, function(taxon){
+                _.forEach(taxa, function (taxon) {
                     taxon.stateIds = _(pairs).filter({taxon: taxon.id}).map('state').value();
                     taxon.html = ko.observable();
-                    if (taxon.widget != undefined) {
-                        http.get(taxon.widget).then(function (response) {
-                            taxon.html(response);
-                        });
-                    }
+                    taxon.url = ko.observable(taxon.widget ? widgetPath + taxon.widget : null);
+
+
+                    //if (taxon.widget != undefined) {
+                    //    http.get(widgetPath + taxon.widget).then(function (response) {
+                    //        console.log("resp", response)
+                    //        taxon.html(response);
+                    //    });
+                    //}
                 });
 
-                _.forEach(characters, function(character){
-                    character.states = _.where(states, {character: character.id});
-                    character.conditions = _(dependencies).where({dependant: character.id}).map('condition').flatten().value();
+                _.forEach(characters, function (character) {
+                    character.states = _.filter(states, {character: character.id});
+                    character.conditions = _(dependencies).filter({dependant: character.id}).map('condition').flatten().value();
 
-                    _.forEach(character.states, function(state){
+                    _.forEach(character.states, function (state) {
                         state.html = ko.observable();
-                        if (state.widget != undefined) {
-                            http.get(state.widget).then(function (response) {
-                                state.html(response);
-                            });
-                        }
+                        state.url = ko.observable(state.widget ? widgetPath + state.widget: null);
+                        //if (state.widget != undefined) {
+                        //    http.get(widgetPath + state.widget).then(function (response) {
+                        //        state.html(response);
+                        //    });
+                        //}
                     })
                 });
 
                 var parents = filterParents(taxa);
-                _.forEach(parents, function(parent){
+                _.forEach(parents, function (parent) {
                     parent.children = [];
-                    _.forEach(taxa, function(item){
-                        if(item.parent === parent.id){
+                    _.forEach(taxa, function (item) {
+                        if (item.parent === parent.id) {
                             parent.children.push(item.id);
                         }
                     });
